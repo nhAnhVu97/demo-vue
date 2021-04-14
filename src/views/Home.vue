af41331d86097072c1d9ab1b6a2935b63d789401
Anh Vu <vu.nha@rga.com>
2021-04-14 10:33:36 +0700

    first init

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/images/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  },
+};
+</script>
